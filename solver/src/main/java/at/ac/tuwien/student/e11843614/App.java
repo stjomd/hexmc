b3a888e66d6bcf12a519f0eca9d18ae397819337
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-03-26 20:51:29 +0200

    Parse DIMACS CNF file

--- solver/src/main/java/at/ac/tuwien/student/e11843614/App.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/App.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.student.e11843614;
 
+import at.ac.tuwien.student.e11843614.cnf.Formula;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
@@ -13,12 +14,14 @@ public class App
         parser.addArgument("input")
             .type(String.class)
             .help("the input path for the DIMACS CNF file");
-
         try {
             Namespace namespace = parser.parseArgs(args);
             String path = namespace.getString("input");
+            Formula formula = Formula.fromPath(path);
         } catch (ArgumentParserException exception) {
             parser.handleError(exception);
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 }
---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/cnf/FormulaParseException.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.student.e11843614.cnf;
+
+public class FormulaParseException extends Exception {
+
+    public FormulaParseException(String message) {
+        super(message);
+    }
+
+    public FormulaParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
