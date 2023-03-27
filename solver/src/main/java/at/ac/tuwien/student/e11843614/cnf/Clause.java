f986ed5a8d24069b05e46b7ca67ddca37c889b66
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-03-27 16:56:22 +0200

    Build incidence graph from formula

--- solver/src/main/java/at/ac/tuwien/student/e11843614/cnf/Clause.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/cnf/Clause.java
@@ -2,18 +2,30 @@ package at.ac.tuwien.student.e11843614.cnf;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * A class representing a clause of a propositional formula.
+ */
 public class Clause {
 
     private final List<Integer> literals = new ArrayList<>();
 
     public Clause() {}
 
-    public Clause(List<Integer> literals) {
-        this.literals.addAll(literals);
-    }
-
+    /**
+     * Adds a literal to the clause.
+     * @param literal a literal represented by an integer (by a negative one if the literal is negated).
+     */
     public void addLiteral(Integer literal) {
         this.literals.add(literal);
+    }
+
+    /**
+     * Retrieves the list of literals in the clause.
+     * @return the list of literals.
+     */
+    public List<Integer> getLiterals() {
+        return literals;
     }
 
     @Override
---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/graph/GraphFactory.java
@@ -0,0 +1,26 @@
+package at.ac.tuwien.student.e11843614.graph;
+
+import at.ac.tuwien.student.e11843614.cnf.Formula;
+
+public abstract class GraphFactory {
+
+    /**
+     * Constructs the incidence graph of the formula.
+     * @param formula the formula to construct the incidence graph for.
+     * @return an incidence graph, where vertices representing variables of the formula end in 1, and vertices
+     *         representing clauses end in 2.
+     */
+    public static Graph incidenceGraph(Formula formula) {
+        // Encode as follows: variable vertices end with 1, clause vertices end with 2
+        Graph graph = new Graph();
+        for (int i = 1; i <= formula.getClauses().size(); i++) {
+            for (Integer literal : formula.getClauses().get(i - 1).getLiterals()) {
+                Integer variableVertex = 10*Math.abs(literal) + 1;
+                Integer clauseVertex = 10*i + 2;
+                graph.addEdge(variableVertex, clauseVertex);
+            }
+        }
+        return graph;
+    }
+
+}
