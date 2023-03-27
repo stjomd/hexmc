86db4cbe2a8f23178e429f94086111378dc56d0e
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-03-27 23:29:27 +0200

    Add documentation

--- solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/BranchDecompositionNode.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/BranchDecompositionNode.java
@@ -3,6 +3,9 @@ package at.ac.tuwien.student.e11843614.decomposition;
 import at.ac.tuwien.student.e11843614.graph.Graph;
 import at.ac.tuwien.student.e11843614.graph.TreeNode;
 
+/**
+ * A class representing a node of the branch decomposition tree, mapped to a graph's edge.
+ */
 public class BranchDecompositionNode extends TreeNode {
 
     private final Graph.Edge edge;
@@ -14,8 +17,17 @@ public class BranchDecompositionNode extends TreeNode {
         this.edge = edge;
     }
 
+    /**
+     * Returns the edge associated with this node.
+     * @return the edge.
+     */
+    public Graph.Edge getEdge() {
+        return edge;
+    }
+
     @Override
     public String toString() {
-        return String.format("[BDNode, %s, children=%s]", edge, getChildren());
+        return String.format("(%s, ch=%s)", edge, getChildren());
     }
+
 }
