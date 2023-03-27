b9178cb3141998380ef69f4a8c500d40af469460
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-03-27 23:29:27 +0200

    Add documentation

--- solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/BranchDecompositionFactory.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/BranchDecompositionFactory.java
@@ -4,6 +4,11 @@ import at.ac.tuwien.student.e11843614.graph.Graph;
 
 public abstract class BranchDecompositionFactory {
 
+    /**
+     * Constructs an approximation of a branch decomposition according to a heuristic.
+     * @param graph the graph to construct a branch decomposition of.
+     * @return the root node of the branch decomposition.
+     */
     public static BranchDecompositionNode heuristic(Graph graph) {
         // Create a star
         BranchDecompositionNode node = new BranchDecompositionNode();
