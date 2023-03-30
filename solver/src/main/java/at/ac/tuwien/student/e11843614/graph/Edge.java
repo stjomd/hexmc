2c6dba2ed02f4a0930b7de27e25f2d6b9fadc082
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-03-30 12:56:47 +0200

    Update documentation for graphs

--- solver/src/main/java/at/ac/tuwien/student/e11843614/graph/Edge.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/graph/Edge.java
@@ -2,16 +2,25 @@ package at.ac.tuwien.student.e11843614.graph;
 
 import java.util.List;
 
+/**
+ * A class that represents an edge between two vertices.
+ * @param <T> the class of this edge's endpoints (vertices).
+ */
 public class Edge<T> {
+
     private final List<T> endpoints;
+
     public Edge (T v, T u) {
         this.endpoints = List.of(v, u);
     }
+
     public List<T> getEndpoints() {
         return endpoints;
     }
+
     @Override
     public String toString() {
         return "<" + endpoints.get(0).toString() + ", " + endpoints.get(1).toString() + ">";
     }
+
 }
