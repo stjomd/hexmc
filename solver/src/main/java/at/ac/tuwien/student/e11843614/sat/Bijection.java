41c59afde030e29e889d8386d38acc10223d64f5
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-02 00:13:54 +0200

    Build formula for branch decomposition SAT encoding

--- solver/src/main/java/at/ac/tuwien/student/e11843614/sat/Bijection.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/sat/Bijection.java
@@ -14,26 +14,58 @@ public class Bijection<S, T> {
     
     private final Map<S, T> forward = new HashMap<>();
     private final Map<T, S> backward = new HashMap<>();
-    
+
+    /**
+     * Adds a pair of elements to the bijection.
+     * @param a an element.
+     * @param b an element.
+     */
     public void put(S a, T b) {
         forward.put(a, b);
         backward.put(b, a);
     }
-    
-    public S get1(T a) {
+
+    /**
+     * Retrieves the element that the specified element is mapped to.
+     * @param a an element.
+     * @return the element mapped to a.
+     */
+    public S getFromDomain(T a) {
         return backward.get(a);
     }
-    
-    public T get2(S a) {
+
+    /**
+     * Retrieves the element that the specified element is mapped to.
+     * @param a an element.
+     * @return the element mapped to a.
+     */
+    public T getFromDestination(S a) {
         return forward.get(a);
     }
 
+    /**
+     * Returns the domain set of this bijection.
+     * @return the domain.
+     */
     public Collection<S> getDomain() {
         return forward.keySet();
     }
 
+    /**
+     * Returns the destination (codomain) set of this bijection.
+     * @return the destination (codomain).
+     */
     public Collection<T> getDestination() {
         return forward.values();
+    }
+
+    /**
+     * Returns the amount of maps in this bijection, irrespective of direction. That is, a mapping 1 <-> 2 amounts to
+     * one map.
+     * @return the size.
+     */
+    public int size() {
+        return forward.size();
     }
 
 }
--- solver/src/main/java/at/ac/tuwien/student/e11843614/sat/SATEncoding.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/sat/SATEncoding.java
@@ -25,7 +25,7 @@ public class SATEncoding {
     }
 
     private int encodeVariable(Variable variable) {
-        Integer value = variables.get2(variable);
+        Integer value = variables.getFromDestination(variable);
         if (value != null) {
             return value;
         }
@@ -103,7 +103,135 @@ public class SATEncoding {
                 }
             }
         }
-        // TODO: add rest
+        // 4
+        for (Integer e : edges.getDestination()) {
+            for (Integer f : edges.getDestination()) {
+                if (e < f) {
+                    for (int i = 1; i < d - 1; i++) {
+                        int var1 = encodeVariable(Variable.leader(e, i));
+                        int var2 = encodeVariable(Variable.leader(f, i));
+                        int var3 = encodeVariable(Variable.set(e, f, i + 1));
+                        int var4 = encodeVariable(Variable.leader(e, i + 1));
+                        int var5 = encodeVariable(Variable.leader(f, i + 1));
+                        formula.addClause(-var1, -var2, -var3, var4, var5);
+                    }
+                }
+            }
+        }
+        // 5
+        for (Integer e : edges.getDestination()) {
+            for (Integer f : edges.getDestination()) {
+                for (Integer g : edges.getDestination()) {
+                    if (e < f && f < g) {
+                        int var1 = encodeVariable(Variable.leader(e, d - 1));
+                        int var2 = encodeVariable(Variable.leader(f, d - 1));
+                        int var3 = encodeVariable(Variable.leader(g, d - 1));
+                        int var4 = encodeVariable(Variable.set(e, f, d));
+                        int var5 = encodeVariable(Variable.set(e, g, d));
+                        int var6 = encodeVariable(Variable.leader(e, d));
+                        int var7 = encodeVariable(Variable.leader(f, d));
+                        int var8 = encodeVariable(Variable.leader(g, d));
+                        formula.addClause(-var1, -var2, -var3, -var4, -var5, var6, var7, var8);
+                    }
+                }
+            }
+        }
+        // 6
+        for (Integer e : edges.getDestination()) {
+            for (int i = 1; i < d; i++) {
+                int var1 = encodeVariable(Variable.leader(e, i));
+                int var2 = encodeVariable(Variable.leader(e, i + 1));
+                formula.addClause(var1, -var2);
+            }
+        }
+        // 7
+        for (Integer e : edges.getDestination()) {
+            for (Integer f : edges.getDestination()) {
+                for (Integer g : edges.getDestination()) {
+                    if (!e.equals(f) && !e.equals(g)) {
+                        Edge<Integer> fEdge = edges.getFromDomain(f);
+                        Edge<Integer> gEdge = edges.getFromDomain(g);
+                        for (Integer u : vertices.getDestination()) {
+                            // Unmap u to check edge endpoints
+                            Integer ud = vertices.getFromDomain(u);
+                            if (fEdge.getEndpoints().contains(ud) && gEdge.getEndpoints().contains(ud)) {
+                                for (int i = 1; i <= d; i++) {
+                                    int var1 = encodeVariable(Variable.leader(e, i));
+                                    int var2 = encodeVariable(Variable.load(e, u, i));
+                                    int var3 = encodeVariable(Variable.set(Math.min(e, f), Math.max(e, f), i));
+                                    int var4 = encodeVariable(Variable.set(Math.min(e, g), Math.max(e, g), i));
+                                    formula.addClause(-var1, var2, var3, -var4);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 8
+        for (Integer e : edges.getDestination()) {
+            for (Integer f : edges.getDestination()) {
+                Edge<Integer> eEdge = edges.getFromDomain(e);
+                Edge<Integer> fEdge = edges.getFromDomain(f);
+                if (!e.equals(f)) {
+                    for (Integer u : vertices.getDestination()) {
+                        // Unmap u to check edge endpoints
+                        Integer ud = vertices.getFromDomain(u);
+                        if (eEdge.getEndpoints().contains(ud) && fEdge.getEndpoints().contains(ud)) {
+                            for (int i = 1; i <= d; i++) {
+                                int var1 = encodeVariable(Variable.leader(e, i));
+                                int var2 = encodeVariable(Variable.set(Math.min(e, f), Math.max(e, f), i));
+                                int var3 = encodeVariable(Variable.load(e, u, i));
+                                formula.addClause(-var1, var2, var3);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 9
+        for (Integer e : edges.getDestination()) {
+            for (Integer u : vertices.getDestination()) {
+                for (int i = 1; i <= d - 2; i++) {
+                    int var1 = encodeVariable(Variable.leader(e, i));
+                    int var2 = encodeVariable(Variable.leader(e, i + 1));
+                    int var3 = encodeVariable(Variable.leader(e, i + 2));
+                    int var4 = encodeVariable(Variable.load(e, u, i));
+                    int var5 = encodeVariable(Variable.load(e, u, i + 2));
+                    int var6 = encodeVariable(Variable.load(e, u, i + 1));
+                    formula.addClause(-var1, -var2, -var3, -var4, -var5, var6);
+                }
+            }
+        }
+        // 10
+        for (Integer e : edges.getDestination()) {
+            for (int u = 2; u <= vertices.size(); u++) {
+                for (int i = 1; i <= d; i++) {
+                    for (int j = 1; j <= w; j++) {
+                        int var1 = encodeVariable(Variable.counter(e, u - 1, i, j));
+                        int var2 = encodeVariable(Variable.counter(e, u, i, j));
+                        int var3 = encodeVariable(Variable.load(e, u, i));
+                        int var4 = encodeVariable(Variable.counter(e, u - 1, i, j - 1));
+                        int var5 = encodeVariable(Variable.counter(e, u, i, j));
+                        int var6 = encodeVariable(Variable.load(e, u, i));
+                        int var7 = encodeVariable(Variable.counter(e, u - 1, i, w));
+                        formula.addClause(-var1, var2);
+                        formula.addClause(-var3, var4, var5);
+                        formula.addClause(-var6, -var7);
+                    }
+                }
+            }
+        }
+        // 11
+        for (Integer e : edges.getDestination()) {
+            for (int u = 1; u <= vertices.size(); u++) {
+                for (int i = 1; i <= d; i++) {
+                    int var1 = encodeVariable(Variable.load(e, u, i));
+                    int var2 = encodeVariable(Variable.counter(e, u, i, 1));
+                    formula.addClause(-var1, var2);
+                }
+            }
+        }
     }
 
     public Formula getFormula() {
