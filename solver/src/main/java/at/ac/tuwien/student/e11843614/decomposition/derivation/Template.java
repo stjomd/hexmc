50785d35552856c51e118da2ce8b60428d45dfff
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-03 20:49:55 +0200

    Add Template class

---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/derivation/Template.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.student.e11843614.decomposition.derivation;
+
+import at.ac.tuwien.student.e11843614.struct.Partition;
+
+public class Template {
+
+    private final Partition<Integer> components = new Partition<>();
+    private final Partition<Integer> groups = new Partition<>();
+
+    public Partition<Integer> getComponents() {
+        return components;
+    }
+
+    public Partition<Integer> getGroups() {
+        return groups;
+    }
+
+}
