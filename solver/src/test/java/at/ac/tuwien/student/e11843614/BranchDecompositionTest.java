7e01849de07cbf857778786f99e609f7a233e5c8
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-05 16:36:13 +0200

    Move instance creation to separate class

--- solver/src/test/java/at/ac/tuwien/student/e11843614/BranchDecompositionTest.java
+++ solver/src/test/java/at/ac/tuwien/student/e11843614/BranchDecompositionTest.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.student.e11843614;
 
 import at.ac.tuwien.student.e11843614.decomposition.BranchDecompositionHeuristic;
 import at.ac.tuwien.student.e11843614.decomposition.BranchDecompositionNode;
+import at.ac.tuwien.student.e11843614.example.GraphExamples;
 import at.ac.tuwien.student.e11843614.graph.Edge;
 import at.ac.tuwien.student.e11843614.graph.Graph;
 import org.junit.jupiter.api.AfterEach;
@@ -24,25 +25,7 @@ public class BranchDecompositionTest {
     @BeforeEach
     public void beforeEach() {
         // a1, b2, c3, d4, e5, f6, g7, h8, i9, j10, k11
-        graph = new Graph<>();
-        graph.addEdge(1, 4); // ad
-        graph.addEdge(1, 8); // ah
-        graph.addEdge(1, 2); // ab
-        graph.addEdge(1, 3); // ac
-        graph.addEdge(4, 5); // de
-        graph.addEdge(2, 5); // be
-        graph.addEdge(2, 6); // bf
-        graph.addEdge(3, 6); // cf
-        graph.addEdge(3, 7); // cg
-        graph.addEdge(7, 8); // gh
-        graph.addEdge(5, 9); // ei
-        graph.addEdge(6, 9); // fi
-        graph.addEdge(6, 10); // fj
-        graph.addEdge(7, 10); // gj
-        graph.addEdge(4, 11); // dk
-        graph.addEdge(8, 11); // hk
-        graph.addEdge(9, 11); // ik
-        graph.addEdge(10, 11); // jk
+        graph = GraphExamples.example();
         heuristic = BranchDecompositionHeuristic.heuristic(graph);
     }
 
