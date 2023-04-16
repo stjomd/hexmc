664e653383cc0a148a53d04ef5805058da4d7112
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-16 16:34:47 +0200

    Add clique decomposition content types

---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/clique/CliqueDecompositionRecoloring.java
@@ -0,0 +1,27 @@
+package at.ac.tuwien.student.e11843614.decomposition.clique;
+
+/**
+ * Contents of a node that represents recoloring.
+ */
+public class CliqueDecompositionRecoloring implements CliqueDecompositionContents {
+
+    private final int from, to;
+
+    public CliqueDecompositionRecoloring(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("recolor(%d, %d)", from, to);
+    }
+}
---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/clique/CliqueDecompositionUnion.java
@@ -0,0 +1,31 @@
+package at.ac.tuwien.student.e11843614.decomposition.clique;
+
+import java.util.Set;
+
+/**
+ * Contents of a node that represents a disjoint union.
+ */
+class CliqueDecompositionUnion implements CliqueDecompositionContents {
+
+    private final Set<Integer> component;
+    private final int level;
+
+    public CliqueDecompositionUnion(Set<Integer> component, int level) {
+        this.component = component;
+        this.level = level;
+    }
+
+    public Set<Integer> getComponent() {
+        return component;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public String toString() {
+        return "union";
+    }
+
+}
