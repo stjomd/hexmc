abc134fbdfe019b5333b88528237a584b134e4d5
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-21 15:22:01 +0200

    Refactor

--- solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/clique/recoloring/RecoloringPossibilityIterator.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/clique/recoloring/RecoloringPossibilityIterator.java
@@ -7,24 +7,27 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * An iterator that iterates over recoloring possibilities of specified length (applicable to one edge in the tree).
+ */
 public class RecoloringPossibilityIterator implements Iterator<List<CliqueRecoloring>> {
 
-    // nodes = [q_1, q_2, ..., q_n]
-    // at each q_i, a recoloring node (i -> j) is possible; i != j; 1 <= i, j <= width.
-    // number (i,j) pair as width*(i-1) + j. Max pair (width, width) corresponds to width^2.
-    // make a list of length n of ints. each int corresponds to the pair of that recoloring node.
-    // increment list like a number. once each int is width^2, we went through all possibilities.
+    // Each recoloring is a pair (i, j) with 1 <= i,j <= k; i != j.
+    // Assign to each pair (i, j) a number k(i-1) + j, then we can iterate over these numbers to obtain a pair.
+    // Maximum value is k^2, corresponding to (j, j).
+    // Let pairList be a list of specified length initialized with ones. Treat it as a number in base k^2.
+    // Then we can increment pairList (just like a number) and go through all recoloring possibilities of given length.
 
-    private final long width;
-    private final List<Long> pairList;
+    private final long k;
+    private final List<Long> list;
 
     private boolean started = false;
 
-    public RecoloringPossibilityIterator(int n, int width) {
-        this.width = width;
-        this.pairList = new ArrayList<>();
+    public RecoloringPossibilityIterator(int n, int k) {
+        this.k = k;
+        this.list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            pairList.add(1L);
+            list.add(1L);
         }
     }
 
@@ -33,8 +36,8 @@ public class RecoloringPossibilityIterator implements Iterator<List<CliqueRecolo
         if (!started) {
             return true;
         } else {
-            for (Long pairNumber : pairList) {
-                if (pairNumber != 1) {
+            for (Long value : list) {
+                if (value != 1) {
                     return true;
                 }
             }
@@ -46,14 +49,14 @@ public class RecoloringPossibilityIterator implements Iterator<List<CliqueRecolo
     public List<CliqueRecoloring> next() {
         if (hasNext()) {
             started = true;
-            List<CliqueRecoloring> list = new ArrayList<>();
-            for (Long pairValue : pairList) {
-                int from = (int) Math.ceil((double) pairValue / width);
-                int to = (int) (pairValue - width * (from - 1));
-                list.add(new CliqueRecoloring(from, to));
+            List<CliqueRecoloring> recolorings = new ArrayList<>();
+            for (Long value : list) {
+                int from = (int) Math.ceil((double) value / k);
+                int to = (int) (value - k * (from - 1));
+                recolorings.add(new CliqueRecoloring(from, to));
             }
-            increment(pairList.size() - 1);
-            return list;
+            increment(list.size() - 1);
+            return recolorings;
         }
         throw new NoSuchElementException("There are no more possibilities");
     }
@@ -64,17 +67,17 @@ public class RecoloringPossibilityIterator implements Iterator<List<CliqueRecolo
         }
         // if value == width^2 it results in i=j, so skip that.
         // look at last value, if its < width^2 - 1, just increment it by 1
-        if (pairList.get(index) < width * width - 1) {
-            pairList.set(index, pairList.get(index) + 1);
+        if (list.get(index) < k * k - 1) {
+            list.set(index, list.get(index) + 1);
             // if i==j, increment once again at same index
-            int i = (int) Math.ceil((double) pairList.get(index) / width);
-            int j = (int) (pairList.get(index) - width*(i - 1));
+            int i = (int) Math.ceil((double) list.get(index) / k);
+            int j = (int) (list.get(index) - k *(i - 1));
             if (i == j) {
                 increment(index);
             }
         } else {
             // otherwise we set it back to 1...
-            pairList.set(index, 1L);
+            list.set(index, 1L);
             // and increment the previous digit.
             increment(index - 1);
         }
