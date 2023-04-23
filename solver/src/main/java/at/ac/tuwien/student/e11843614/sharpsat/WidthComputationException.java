2aec26193201668e94b7a710131475fa508815ca
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-23 23:09:04 +0200

    Add #SAT class

---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/sharpsat/WidthComputationException.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.student.e11843614.sharpsat;
+
+public class WidthComputationException extends Exception {
+
+    public WidthComputationException(String message) {
+        super(message);
+    }
+
+    public WidthComputationException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+}
