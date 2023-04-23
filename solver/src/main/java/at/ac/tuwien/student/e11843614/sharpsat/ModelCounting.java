24725576bc2c8d41ea5340bb4c9f6312f7b7db5d
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-04-23 23:16:15 +0200

    Compute decompositions in #SAT class

--- solver/src/main/java/at/ac/tuwien/student/e11843614/sharpsat/ModelCounting.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/sharpsat/ModelCounting.java
@@ -1,8 +1,10 @@
 package at.ac.tuwien.student.e11843614.sharpsat;
 
 import at.ac.tuwien.student.e11843614.Logger;
+import at.ac.tuwien.student.e11843614.decomposition.DecompositionFactory;
 import at.ac.tuwien.student.e11843614.decomposition.carving.CarvingDerivation;
 import at.ac.tuwien.student.e11843614.decomposition.clique.CliqueDerivation;
+import at.ac.tuwien.student.e11843614.decomposition.clique.operation.CliqueOperation;
 import at.ac.tuwien.student.e11843614.formula.Formula;
 import at.ac.tuwien.student.e11843614.sat.SATEncoding;
 import at.ac.tuwien.student.e11843614.sat.SATSolver;
@@ -10,6 +12,7 @@ import at.ac.tuwien.student.e11843614.sat.Variable;
 import at.ac.tuwien.student.e11843614.sat.factory.SATEncodingFactory;
 import at.ac.tuwien.student.e11843614.struct.graph.Graph;
 import at.ac.tuwien.student.e11843614.struct.graph.GraphFactory;
+import at.ac.tuwien.student.e11843614.struct.tree.TreeNode;
 import org.sat4j.specs.TimeoutException;
 
 import java.util.Set;
@@ -34,6 +37,7 @@ public abstract class ModelCounting {
         if (derivation == null) {
             throw new WidthComputationException("Could not determine carving-width");
         }
+        TreeNode<Integer> decomposition = DecompositionFactory.carving(derivation);
         // TODO: dynamic algorithm for #SAT
         Logger.warn("#SAT utilizing psw not yet implemented, returning 0");
         return 0;
@@ -43,8 +47,9 @@ public abstract class ModelCounting {
     public static int cw(Formula formula) throws TimeoutException, WidthComputationException {
         Graph incidenceGraph = GraphFactory.incidenceGraph(formula);
         // Determine clique-width
+        // TODO: special case check for cw = 1
         CliqueDerivation derivation = null;
-        for (int w = 1; w <= incidenceGraph.vertices().size(); w++) {
+        for (int w = 2; w <= incidenceGraph.vertices().size(); w++) {
             SATEncoding encoding = SATEncodingFactory.forCliqueWidth(incidenceGraph, w);
             Set<Variable> truths = SATSolver.getSatisfyingAssignment(encoding);
             if (!truths.isEmpty()) {
@@ -56,6 +61,7 @@ public abstract class ModelCounting {
         if (derivation == null) {
             throw new WidthComputationException("Could not determine clique-width");
         }
+        TreeNode<CliqueOperation> decomposition = DecompositionFactory.clique(derivation, incidenceGraph);
         // TODO: dynamic algorithm for #SAT
         Logger.warn("#SAT utilizing cw not yet implemented, returning 0");
         return 0;
