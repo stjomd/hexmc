d87c5f0208337183144e84dfb60596d363d59b76
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-16 23:04:06 +0200

    Change color of debug messages
    Factor in --verbose when exceptions occur
    
    Refactor constants setup
    
    Handle --version correctly

--- solver/src/main/java/at/ac/tuwien/student/e11843614/Constants.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/Constants.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.student.e11843614;
 
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public abstract class Constants {
 
     public enum Parameter {
@@ -11,32 +13,27 @@ public abstract class Constants {
     private static int timeout = 0;
     private static boolean verbose = false;
 
+    public static void set(Namespace namespace) {
+        algorithm = namespace.get("alg");
+        carving = namespace.getBoolean("carving");
+        timeout = namespace.getInt("timeout");
+        verbose = namespace.getBoolean("verbose");
+    }
+
     public static Parameter algorithm() {
         return algorithm;
-    }
-    public static void setAlgorithm(Parameter parameter) {
-        Constants.algorithm = parameter;
     }
 
     public static boolean carving() {
         return carving;
     }
-    public static void setCarving(boolean flag) {
-        Constants.carving = flag;
-    }
 
     public static int timeout() {
         return timeout;
     }
-    public static void setTimeout(int seconds) {
-        Constants.timeout = seconds;
-    }
 
     public static boolean verbose() {
         return verbose;
-    }
-    public static void setVerbose(boolean flag) {
-        Constants.verbose = flag;
     }
 
 }
