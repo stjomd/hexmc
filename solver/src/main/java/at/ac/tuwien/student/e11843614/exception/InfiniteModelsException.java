ee96b08546164afe8c1cdd63008a729118ec7bb4
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-16 11:27:33 +0200

    Move general count method into #SAT class

---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/exception/InfiniteModelsException.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.student.e11843614.exception;
+
+public class InfiniteModelsException extends Exception {
+
+    public InfiniteModelsException() {
+    }
+
+    public InfiniteModelsException(String message) {
+        super(message);
+    }
+
+    public InfiniteModelsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
