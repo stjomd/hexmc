e6dd5317aed8ee646091d8c08168a0c58ef2f3f5
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-16 20:56:22 +0200

    Remove unnecessary code

--- solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/carving/CarvingDerivation.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/decomposition/carving/CarvingDerivation.java
@@ -4,11 +4,8 @@ import at.ac.tuwien.student.e11843614.Logger;
 import at.ac.tuwien.student.e11843614.sat.SATEncoding;
 import at.ac.tuwien.student.e11843614.sat.Variable;
 import at.ac.tuwien.student.e11843614.struct.Partition;
-import at.ac.tuwien.student.e11843614.struct.graph.Edge;
-import at.ac.tuwien.student.e11843614.struct.graph.Graph;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -45,34 +42,6 @@ public class CarvingDerivation {
     }
 
     /**
-     * Computes the width of this derivation.
-     * @param graph the corresponding graph.
-     * @return the width.
-     */
-    public int getWidth(Graph graph) {
-        // Let V be an equivalence class from any level of the derivation. Let delta(V) denote the set of edges in
-        // the corresponding graph that have one endpoint in V and one endpoint outside V. Then the width of this
-        // derivation is the maximum |delta(V)| over all equivalence classes V.
-        int width = 0;
-        for (Partition<Integer> partition : derivation) {
-            for (Set<Integer> ec : partition.equivalenceClasses()) {
-                Set<Edge> edges = new HashSet<>();
-                for (Edge edge : graph.edges()) {
-                    int u = edge.endpoints().get(0);
-                    int v = edge.endpoints().get(1);
-                    if (ec.contains(u) && !ec.contains(v)) {
-                        edges.add(edge);
-                    } else if (ec.contains(v) && !ec.contains(u)) {
-                        edges.add(edge);
-                    }
-                }
-                width = Math.max(width, edges.size());
-            }
-        }
-        return width;
-    }
-
-    /**
      * Fills the derivation's partitions.
      * @param assignment the satisfying assignment, the set of variables set to true.
      * @param sat the SAT encoding.
@@ -104,43 +73,6 @@ public class CarvingDerivation {
             }
         }
         Logger.debug("Constructed a derivation for carving-width with l = " + size());
-    }
-
-    /**
-     * Checks whether this derivation fulfils the conditions.
-     * @param graph the graph associated with this derivation.
-     * @return true, if all conditions are satisfied, and false otherwise.
-     */
-    public boolean fulfilsConditions(Graph graph) {
-        int l = derivation.size();
-        // D1
-        // P_1 has |V(G)| equivalence classes, each consisting of one element
-        if (getLevel(1).size() != graph.vertices().size()) {
-            return false;
-        }
-        for (Set<Integer> ec : getLevel(1).equivalenceClasses()) {
-            if (ec.size() != 1) {
-                return false;
-            }
-        }
-        // P_l has 1 equivalence class which contains all vertices
-        if (getLevel(l).size() != 1) {
-            return false;
-        }
-        if (!getLevel(l).equivalenceClasses().iterator().next().containsAll(graph.vertices())) {
-            return false;
-        }
-        // D2
-        for (int i = 1; i < l - 2; i++) {
-            if (!getLevel(i).isBinaryRefinementOf(getLevel(i + 1))) {
-                return false;
-            }
-        }
-        // D3
-        if (!getLevel(l - 1).isTernaryRefinementOf(getLevel(l))) {
-            return false;
-        }
-        return true;
     }
 
     @Override
