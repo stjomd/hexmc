21b2b6850aa25e8e12f41e9e21c2e80146767f41
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-16 17:27:25 +0200

    Add algorithm argument

--- solver/src/main/java/at/ac/tuwien/student/e11843614/Constants.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/Constants.java
@@ -2,14 +2,19 @@ package at.ac.tuwien.student.e11843614;
 
 public abstract class Constants {
 
-    private static boolean verbose = false;
-    private static int timeout = 0;
-
-    public static boolean verbose() {
-        return verbose;
+    public enum Parameter {
+        psw, cw
     }
-    public static void setVerbose(boolean flag) {
-        Constants.verbose = flag;
+
+    private static Parameter algorithm = Parameter.psw;
+    private static int timeout = 0;
+    private static boolean verbose = false;
+
+    public static Parameter algorithm() {
+        return algorithm;
+    }
+    public static void setAlgorithm(Parameter parameter) {
+        Constants.algorithm = parameter;
     }
 
     public static int timeout() {
@@ -17,6 +22,13 @@ public abstract class Constants {
     }
     public static void setTimeout(int seconds) {
         Constants.timeout = seconds;
+    }
+
+    public static boolean verbose() {
+        return verbose;
+    }
+    public static void setVerbose(boolean flag) {
+        Constants.verbose = flag;
     }
 
 }
