0a93a73375b73e1831af4b3b33be75165be54f5d
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-20 22:17:36 +0200

    Move argument parser setup to separate class

---
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/ArgumentParserFactory.java
@@ -0,0 +1,47 @@
+package at.ac.tuwien.student.e11843614;
+
+import at.ac.tuwien.student.e11843614.sharpsat.ModelCounting;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+
+import java.util.Locale;
+
+public abstract class ArgumentParserFactory {
+
+    public static ArgumentParser parser(String name, String version) {
+        ArgumentParser parser = ArgumentParsers.newFor(name)
+            .locale(Locale.US)
+            .singleMetavar(true)
+            .build()
+            .version(version)
+            .description("Accepts a CNF formula in DIMACS CNF format, and counts the number of its models.");
+        parser.addArgument("input")
+            .type(String.class)
+            .help("the input path for the DIMACS CNF file");
+        parser.addArgument("--version")
+            .action(Arguments.version())
+            .help("output the version and exit");
+        parser.addArgument("-a", "--alg")
+            .type(ModelCounting.Algorithm.class)
+            .setDefault(ModelCounting.Algorithm.psw)
+            .help("specifies the algorithm to use for model counting (either utilizing ps-width or clique-width)."
+                + " WARNING! cw is experimental and does not return correct answers, use it only for debugging."
+                + " The standard value is psw (ps-width)");
+        parser.addArgument("-c", "--carving")
+            .type(boolean.class)
+            .action(Arguments.storeTrue())
+            .help("use a carving decomposition as input for the psw algorithm. By default computes a decomposition heuristically");
+        parser.addArgument("-t", "--timeout")
+            .metavar("SECONDS")
+            .type(int.class)
+            .setDefault(0)
+            .help("timeout in seconds for the SAT solver");
+        parser.addArgument("-v", "--verbose")
+            .type(boolean.class)
+            .action(Arguments.storeTrue()) // defaults to... false
+            .help("output additional information");
+        return parser;
+    }
+
+}
--- solver/src/main/java/at/ac/tuwien/student/e11843614/Main.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/Main.java
@@ -3,15 +3,12 @@ package at.ac.tuwien.student.e11843614;
 import at.ac.tuwien.student.e11843614.exception.InfiniteModelsException;
 import at.ac.tuwien.student.e11843614.formula.Formula;
 import at.ac.tuwien.student.e11843614.sharpsat.ModelCounting;
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import org.sat4j.specs.TimeoutException;
 
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Properties;
 
 import static java.lang.System.exit;
@@ -23,39 +20,15 @@ public class Main {
         Properties properties = new Properties();
         try {
             properties.load(Main.class.getClassLoader().getResourceAsStream("project.properties"));
-        } catch (IOException ignored) {}
+        } catch (IOException exception) {
+            Logger.error("Unable to load the properties file");
+            exception.printStackTrace();
+            exit(1);
+        }
 
-        ArgumentParser parser = ArgumentParsers.newFor(properties.getProperty("name"))
-            .locale(Locale.US)
-            .singleMetavar(true)
-            .build()
-            .version(properties.getProperty("version"))
-            .description("Accepts a CNF formula in DIMACS CNF format, and counts the number of its models.");
-        parser.addArgument("input")
-            .type(String.class)
-            .help("the input path for the DIMACS CNF file");
-        parser.addArgument("--version")
-            .action(Arguments.version())
-            .help("output the version and exit");
-        parser.addArgument("-a", "--alg")
-            .type(ModelCounting.Algorithm.class)
-            .setDefault(ModelCounting.Algorithm.psw)
-            .help("specifies the algorithm to use for model counting (either utilizing ps-width or clique-width)."
-                + " WARNING! cw is experimental and does not return correct answers, use it only for debugging."
-                + " The standard value is psw (ps-width)");
-        parser.addArgument("-c", "--carving")
-            .type(boolean.class)
-            .action(Arguments.storeTrue())
-            .help("use a carving decomposition as input for the psw algorithm. By default computes a decomposition heuristically");
-        parser.addArgument("-t", "--timeout")
-            .metavar("SECONDS")
-            .type(int.class)
-            .setDefault(0)
-            .help("timeout in seconds for the SAT solver");
-        parser.addArgument("-v", "--verbose")
-            .type(boolean.class)
-            .action(Arguments.storeTrue()) // defaults to... false
-            .help("output additional information");
+        ArgumentParser parser = ArgumentParserFactory.parser(
+            properties.getProperty("name"), properties.getProperty("version")
+        );
 
         try {
             // Parse arguments
