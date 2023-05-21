23921b3f0ad1b68fe21482ab74f529f479d9382b
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-21 16:35:42 +0200

    Run solver, parse output

--- solver/benchmarks/generate.py
+++ solver/benchmarks/generate.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 import random
 
 max_width = 10
-temp_folder = "temp"
+temp_path = pathlib.Path(__file__).parent / "temp"
+solver_path = pathlib.Path(__file__).parent.parent / "hexmc"
 
 def construct_formula(variables, clauses):
     formula = []
@@ -37,7 +39,7 @@ def construct_formula(variables, clauses):
     return formula
 
 def write_formula(formula, id, variables):
-    path = temp_folder + "/" + str(id) + ".cnf"
+    path = temp_path / (str(id) + ".cnf")
     with open(path, "w") as file:
         file.write("p cnf " + str(variables) + " " + str(len(formula)) + "\n")
         for clause in formula:
@@ -45,8 +47,26 @@ def write_formula(formula, id, variables):
             string += " 0\n"
             file.write(string)
 
+def run_solver(id):
+    input_path = temp_path / (str(id) + ".cnf")
+    command = '"' + str(solver_path) + '" "' + str(input_path) + '" --verbose'
+    stream = os.popen(command)
+    output = stream.readlines()
+    # Parse output
+    width = -1
+    time = ""
+    for line in output:
+        trimmed = line[5:-5]
+        if trimmed.startswith("ps-width of the decomposition is"):
+            width = int(trimmed.split()[-1])
+        elif trimmed.startswith("[psw] Time elapsed:"):
+            time = trimmed.split()[-1]
+    print(width)
+    print(time)
+
 if __name__ == "__main__":
-    if not os.path.exists(temp_folder):
-        os.makedirs(temp_folder)
+    if not os.path.exists(temp_path):
+        os.makedirs(temp_path)
     formula = construct_formula(12, 3)
     write_formula(formula, 1, 12)
+    run_solver(1)
