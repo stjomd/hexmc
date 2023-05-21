94041d8eccefdf1a0edf0f618182f78b82ae08a4
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-22 00:24:51 +0200

    Print runtime when errors occur

--- solver/src/main/java/at/ac/tuwien/student/e11843614/Main.java
+++ solver/src/main/java/at/ac/tuwien/student/e11843614/Main.java
@@ -5,13 +5,17 @@ import at.ac.tuwien.student.e11843614.counting.ModelCounting;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
 
+    private static final StopWatch stopwatch = new StopWatch();
+
     public static void main(String[] args) {
+        stopwatch.start();
         // Load properties file
         Properties properties = new Properties();
         try {
@@ -38,14 +42,15 @@ public class Main {
             parser.handleError(exception);
             System.exit(1);
         } catch (ArithmeticException exception) {
-            Logger.debug("Long overflow occurred");
+            Logger.error("Long overflow occurred");
             Logger.info(">= " + Long.MAX_VALUE);
             gracefulExit(exception);
-        } catch (Exception exception) {
+        } catch (Throwable exception) {
             Logger.error(exception.getMessage());
             gracefulExit(exception);
         }
         // Exit successfully
+        stopwatch.stop();
         System.exit(0);
     }
 
@@ -53,7 +58,9 @@ public class Main {
      * Prints the stack trace in verbose mode, and exists with exit code 1.
      * @param exception the exception.
      */
-    private static void gracefulExit(Exception exception) {
+    private static void gracefulExit(Throwable exception) {
+        stopwatch.stop();
+        Logger.debug("Total runtime: " + stopwatch.formatTime());
         if (exception != null && Constants.verbose()) {
             exception.printStackTrace();
         }
