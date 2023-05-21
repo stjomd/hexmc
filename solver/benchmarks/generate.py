6cfa101b836f47539e3bdc7f26cac60ff728d275
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-21 17:47:02 +0200

    Generate formulas and write into files

--- solver/benchmarks/generate.py
+++ solver/benchmarks/generate.py
@@ -1,11 +1,17 @@
 import os
-import pathlib
+from pathlib import Path
 import random
+import sys
 
-max_width = 10
-temp_path = pathlib.Path(__file__).parent / "temp"
-solver_path = pathlib.Path(__file__).parent.parent / "hexmc"
+# Amount of formulas generated for each (n, m)
+tries_per_combination = 5
 
+# Paths (don't have to be changed if project structure not changed)
+temp_path = Path(__file__).parent / "temp"
+instances_path = Path(__file__).parent / "instances"
+solver_path = Path(__file__).parent.parent / "hexmc"
+
+# Constructs a random formula
 def construct_formula(variables, clauses):
     formula = []
     for m in range(clauses):
@@ -38,18 +44,18 @@ def construct_formula(variables, clauses):
         formula[clause_index].append(sign * variable)
     return formula
 
-def write_formula(formula, file_name, variables):
-    path = temp_path / file_name
+# Writes the formula into a DIMACS format file
+def write_formula(formula, path, variables, clauses):
     with open(path, "w") as file:
-        file.write("p cnf " + str(variables) + " " + str(len(formula)) + "\n")
+        file.write("p cnf " + str(variables) + " " + str(clauses) + "\n")
         for clause in formula:
             string = ' '.join(str(x) for x in clause)
             string += " 0\n"
             file.write(string)
 
-def run_solver(file_name):
-    input_path = temp_path / file_name
-    command = '"' + str(solver_path) + '" "' + str(input_path) + '" --verbose'
+# Runs the solver, parses the output
+def run_solver(path):
+    command = '"' + str(solver_path) + '" "' + str(path) + '" --verbose'
     stream = os.popen(command)
     output = stream.readlines()
     # Parse output
@@ -65,8 +71,44 @@ def run_solver(file_name):
     return [width, time, models]
 
 if __name__ == "__main__":
+    # Parse arguments
+    size = 0
+    if len(sys.argv) != 2:
+        print("usage: python3 generate.py <size>")
+        exit(1)
+    else:
+        size = int(sys.argv[1])
+    # Create directories
     if not os.path.exists(temp_path):
         os.makedirs(temp_path)
-    formula = construct_formula(12, 3)
-    write_formula(formula, "1.cnf", 12)
-    run_solver("1.cnf")
+    if not os.path.exists(instances_path):
+        os.makedirs(instances_path)
+    temp_file = temp_path / "temp.cnf"
+    # Create a dict where we store how many instances for specific ps-width we obtained
+    progress = {}
+    # n is the amount of variables, m is the amount of clauses
+    for n in range(2, size):
+        for m in range(1, size):
+            for i in range(tries_per_combination):
+                formula = construct_formula(n, m)
+                # Create a temporary file, and run solver on it
+                write_formula(formula, temp_file, n, m)
+                width, time, models = run_solver(temp_file)
+                # Record in progress dict
+                if not width in progress:
+                    progress[width] = 0
+                progress[width] += 1
+                # Save to instances folder
+                path = instances_path / str(width)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                file_name = "psw-" + str(width) + "-order-" + str(progress[width]) + ".cnf"
+                write_formula(formula, path / file_name, n, m)
+                # Output to console
+                print("n = {}, m = {}, i = {}: decomposition had ps-width {}, elapsed time was {}".format(n, m, i, width, time))
+    # Delete temporary files
+    if os.path.isfile(temp_file):
+        os.remove(temp_file)
+    if os.path.isdir(temp_path):
+        os.rmdir(temp_path)
+    print("Done.")
