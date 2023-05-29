b220ced370b608850db74b4013f09eb99f00fcc2
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-05-29 19:19:59 +0200

    Use report parsing function

--- solver/benchmarks/plot.py
+++ solver/benchmarks/plot.py
@@ -18,16 +18,13 @@ class DataType(enum.Enum):
 class SolverRun:
     n, m = None, None
     runtime = None
-    ps_width = None
+    width = None
     models = None
     memory = None
     error = None
     def __init__(self, n, m):
         self.n = n
         self.m = m
-    def __repr__(self):
-        return "Run({}, {}; time = {}, psw = {}, models = {}, memory = {}, error = {})" \
-            .format(self.n, self.m, self.runtime, self.ps_width, self.models, self.memory, self.error)
 
 def timestr_to_seconds(string):
     units = [float(x) for x in string.split(":")]
@@ -60,7 +57,7 @@ def parse_reports(reports_path, action):
                     i = 0
                     for width in items[2:]:
                         if width != "unknown":
-                            runs[i].ps_width = int(width)
+                            runs[i].width = int(width)
                         i += 1
                 elif line.startswith("models"):
                     # Model amount line
@@ -89,36 +86,17 @@ def parse_reports(reports_path, action):
 
 # ----- Plots -------------------------------------------------
 
-def psw_heatmap(instances_folder, save_name):
-    indices = {}
-    # In data[n][m], store the ps-widths
-    data = np.zeros((31, 101, 5))
-    for subdir in os.listdir(instances_folder):
-        if subdir == ".DS_Store":
-            continue
-        # subdir: ps-width (no failed instances in this set)
-        for instance in os.listdir(instances_folder/subdir):
-            if not instance.endswith(".cnf"):
-                continue
-            with open(instances_folder/subdir/instance, "r") as instance_file:
-                header = instance_file.readlines()[0].split()
-                n, m = int(header[2]), int(header[3])
-                if n not in indices:
-                    indices[n] = {}
-                if m not in indices[n]:
-                    indices[n][m] = 0
-                indices[n][m] += 1
-                data[n, m, indices[n][m] - 1] = int(subdir)
-    # Reduce the array of ps-widths to a single value
+def psw_heatmap(reports_path, save_name):
+    # Define function passed into parse_reports
     table = np.zeros((31, 101))
     table.fill(np.nan)
-    for x in range(31):
-        for y in range(101):
-            if x < 2 or y < 1:
-                table[x, y] = 0 # placeholder, not plotted
-            else:
-                table[x, y] = math.log(data[x, y, 2]) # log of median
-    # Use matplotlib to make a plot
+    def fill_table(n, m, runs):
+        widths = [run.width for run in runs]
+        average = math.log(sum(widths) / len(widths))
+        table[n, m] = average
+    # Parse reports
+    parse_reports(reports_path, fill_table)
+    # Matplotlib
     fig, ax = plt.subplots(1, 1)
     heatmap = plt.imshow(table, cmap='viridis')
     # Add single ticks to left bottom for 1/2
@@ -138,42 +116,30 @@ def psw_heatmap(instances_folder, save_name):
     # Save
     plt.savefig(graphics_dir/save_name, bbox_inches = 'tight')
 
-def data_of_clauses(instances_folder, fixed_ns, max_clauses, save_name, xticks = None, data_type = DataType.runtime):
+def data_of_clauses(reports_path, fixed_ns, max_clauses, save_name, xticks = None, data_type = DataType.runtime):
+    # Define function passed into parse_reports
     data = {}
-    for subdir in os.listdir(instances_folder):
-        if subdir == ".DS_Store":
-            continue
-        # subdir: ps-width (no failed instances in this set)
-        for instance in os.listdir(instances_folder/subdir):
-            if not instance.endswith(".cnf"):
-                continue
-            with open(instances_folder/subdir/instance, "r") as instance_file:
-                lines = instance_file.readlines()
-                header = lines[0].split()
-                n, m = int(header[2]), int(header[3])
-                time = 0.0
-                memory = 0.0
-                for line in lines:
-                    if line.startswith("c time:"):
-                        time = timestr_to_seconds(line.split()[-1])
-                    elif line.startswith("c peak memory usage:"):
-                        memory = float(line.split()[-2])
-                # Write to data
-                if n not in data:
-                    data[n] = {}
-                if m not in data[n]:
-                    data[n][m] = []
-                if data_type == DataType.runtime:
-                    data[n][m].append(time)
-                elif data_type == DataType.memory:
-                    data[n][m].append(memory)
-    # Reduce the array of values to a single value
+    def fill_table(n, m, runs):
+        values = []
+        if data_type == DataType.runtime:
+            values = [run.runtime for run in runs]
+        elif data_type == DataType.memory:
+            values = [run.memory for run in runs]
+        average = sum(values) / len(values)
+        if n not in data:
+            data[n] = {}
+        if m not in data[n]:
+            data[n][m] = 0
+        data[n][m] = average
+    # Parse reports
+    parse_reports(reports_path, fill_table)
+    # Matplotlib
     fig, ax = plt.subplots(1, 1)
     for n in fixed_ns:
         table = np.zeros((max_clauses+1,))
         table.fill(np.nan)
         for m in range(1, max_clauses+1):
-            table[m] = sum(data[n][m])/len(data[n][m]) # average
+            table[m] = data[n][m]
         plt.plot(np.arange(max_clauses+1), table, label = r'$n$ = {}'.format(n), marker = '.')
     plt.legend(loc = 'upper left')
     plt.xlabel(r'$m$')
@@ -186,42 +152,30 @@ def data_of_clauses(instances_folder, fixed_ns, max_clauses, save_name, xticks =
     # Save
     plt.savefig(graphics_dir/save_name)
 
-def data_of_variables(instances_folder, fixed_ms, max_variables, save_name, xticks = None, data_type = DataType.runtime):
+def data_of_variables(reports_path, fixed_ms, max_variables, save_name, xticks = None, data_type = DataType.runtime):
+    # Define function passed into parse_reports
     data = {}
-    for subdir in os.listdir(instances_folder):
-        if subdir == ".DS_Store":
-            continue
-        # subdir: ps-width (no failed instances in this set)
-        for instance in os.listdir(instances_folder/subdir):
-            if not instance.endswith(".cnf"):
-                continue
-            with open(instances_folder/subdir/instance, "r") as instance_file:
-                lines = instance_file.readlines()
-                header = lines[0].split()
-                n, m = int(header[2]), int(header[3])
-                time = 0.0
-                memory = 0.0
-                for line in lines:
-                    if line.startswith("c time:"):
-                        time = timestr_to_seconds(line.split()[-1])
-                    elif line.startswith("c peak memory usage:"):
-                        memory = float(line.split()[-2])
-                # Write to data
-                if n not in data:
-                    data[n] = {}
-                if m not in data[n]:
-                    data[n][m] = []
-                if data_type == DataType.runtime:
-                    data[n][m].append(time)
-                elif data_type == DataType.memory:
-                    data[n][m].append(memory)
-    # Reduce the array of values to a single value
+    def fill_table(n, m, runs):
+        values = []
+        if data_type == DataType.runtime:
+            values = [run.runtime for run in runs]
+        elif data_type == DataType.memory:
+            values = [run.memory for run in runs]
+        average = sum(values) / len(values)
+        if n not in data:
+            data[n] = {}
+        if m not in data[n]:
+            data[n][m] = 0
+        data[n][m] = average
+    # Parse reports
+    parse_reports(reports_path, fill_table)
+    # Matplotlib
     fig, ax = plt.subplots(1, 1)
     for m in fixed_ms:
         table = np.zeros((max_variables + 1,))
         table.fill(np.nan)
         for n in range(2, max_variables + 1):
-            table[n] = sum(data[n][m])/len(data[n][m])
+            table[n] = data[n][m]
         plt.plot(np.arange(max_variables + 1), table, label = r'$m$ = {}'.format(m), marker = '.')
     plt.legend(loc = 'upper left')
     plt.xlabel(r'$n$')
@@ -234,45 +188,33 @@ def data_of_variables(instances_folder, fixed_ms, max_variables, save_name, xtic
     # Save
     plt.savefig(graphics_dir/save_name)
 
-def data_of_psw(instances_folder, save_name, xticks = None, data_type = DataType.runtime):
+def data_of_psw(reports_path, save_name, xticks = None, data_type = DataType.runtime):
+    # Define function passed into parse_reports
     data = {}
+    def fill_table(n, m, runs):
+        values = []
+        widths = [run.width for run in runs]
+        if data_type == DataType.runtime:
+            values = [run.runtime for run in runs]
+        elif data_type == DataType.memory:
+            values = [run.memory for run in runs]
+        for i in range(len(runs)):
+            k, value = widths[i], values[i]
+            if k not in data:
+                data[k] = []
+            data[k].append(value)
+    # Parse reports
+    parse_reports(reports_path, fill_table)
+    # Determine max ps-width
     max_psw = 0
-    for subdir in os.listdir(instances_folder):
-        if subdir == ".DS_Store":
-            continue
-        elif subdir == "5099": # exclude outlier
-            continue
-        if subdir.isdigit():
-            max_psw = max(max_psw, int(subdir))
-        # subdir: ps-width (no failed instances in this set)
-        for instance in os.listdir(instances_folder/subdir):
-            if not instance.endswith(".cnf"):
-                continue
-            with open(instances_folder/subdir/instance, "r") as instance_file:
-                lines = instance_file.readlines()
-                header = lines[0].split()
-                n, m = int(header[2]), int(header[3])
-                time = 0.0
-                memory = 0.0
-                for line in lines:
-                    if line.startswith("c time:"):
-                        time = timestr_to_seconds(line.split()[-1])
-                    elif line.startswith("c peak memory usage:"):
-                        memory = float(line.split()[-2])
-                # Write to data
-                psw = int(subdir)
-                if psw not in data:
-                    data[psw] = []
-                if data_type == DataType.runtime:
-                    data[psw].append(time)
-                elif data_type == DataType.memory:
-                    data[psw].append(memory)
-    y = np.full((max_psw+1,), np.nan)
-    # Fill table
     for k in data:
-        y[k] = sum(data[k])/len(data[k])
+        max_psw = max(max_psw, k)
+    # Matplotlib
+    table = np.full((max_psw+1,), np.nan)
+    for k in data:
+        table[k] = sum(data[k]) / len(data[k])
     fig, ax = plt.subplots(1, 1)
-    plt.scatter(np.arange(max_psw+1), y, marker = '.')
+    plt.scatter(np.arange(max_psw+1), table, marker = '.')
     plt.xlabel(r'$k$')
     if data_type == DataType.runtime:
         plt.ylabel('average runtime (seconds)')
@@ -293,19 +235,19 @@ if __name__ == "__main__":
 
     # Call one function at a time
 
-    # ----- set: instances; runtime -----
-    # psw_heatmap(main_dir/"instances", "heat_k_from_nm.pdf")
-    # data_of_clauses(main_dir/"instances", [20, 25, 30], 100, "runtime_of_clauses.pdf", xticks = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-    # data_of_variables(main_dir/"instances", [50, 60, 75, 100], 30, "runtime_of_variables.pdf", xticks = [2, 5, 10, 15, 20, 25, 30])
-    # data_of_psw(main_dir/"instances", "runtime_of_psw.pdf", xticks = [2, 500, 1000, 1500, 2000, 2500, 3000])
-    # ----- set: instances; memory -----
-    # data_of_clauses(main_dir/"instances", [20, 25, 30], 100, "memory_of_clauses.pdf", data_type = DataType.memory, xticks = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-    # data_of_variables(main_dir/"instances", [50, 60, 75, 100], 30, "memory_of_variables.pdf", data_type = DataType.memory, xticks = [2, 5, 10, 15, 20, 25, 30])
-    # data_of_psw(main_dir/"instances", "memory_of_psw.pdf", data_type = DataType.memory, xticks = [2, 500, 1000, 1500, 2000, 2500, 3000])
+    # ----- set: bm; runtime -----
+    # psw_heatmap(main_dir/"reports", "heat_k_from_nm.pdf")
+    # data_of_clauses(main_dir/"reports", [20, 25, 30], 100, "runtime_of_clauses.pdf", xticks = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    # data_of_variables(main_dir/"reports", [50, 60, 75, 100], 30, "runtime_of_variables.pdf", xticks = [2, 5, 10, 15, 20, 25, 30])
+    # data_of_psw(main_dir/"reports", "runtime_of_psw.pdf", xticks = [2, 500, 1000, 1500, 2000, 2500, 3000])
+    # ----- set: bm; memory -----
+    # data_of_clauses(main_dir/"reports", [20, 25, 30], 100, "memory_of_clauses.pdf", data_type = DataType.memory, xticks = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    # data_of_variables(main_dir/"reports", [50, 60, 75, 100], 30, "memory_of_variables.pdf", data_type = DataType.memory, xticks = [2, 5, 10, 15, 20, 25, 30])
+    # data_of_psw(main_dir/"reports", "memory_of_psw.pdf", data_type = DataType.memory, xticks = [2, 500, 1000, 1500, 2000, 2500, 3000])
 
-    # ----- set: instances-n63; runtime -----
-    # data_of_clauses(main_dir/"instances-n63", [63], 50, "runtime_of_clauses-n63.pdf", xticks = [1, 10, 20, 30, 40, 50])
-    # data_of_psw(main_dir/"instances-n63", "runtime_of_psw-n63.pdf", xticks = [2, 200, 400, 600, 800, 1000, 1200, 1400])
-    # ----- set: instances-n63; memory -----
-    # data_of_clauses(main_dir/"instances-n63", [63], 50, "memory_of_clauses-n63.pdf", data_type = DataType.memory, xticks = [1, 10, 20, 30, 40, 50])
-    # data_of_psw(main_dir/"instances-n63", "memory_of_psw-n63.pdf", data_type = DataType.memory, xticks = [2, 200, 400, 600, 800, 1000, 1200, 1400])
+    # ----- set: bm-n-63; runtime -----
+    # data_of_clauses(main_dir/"reports-n63", [63], 50, "runtime_of_clauses-n63.pdf", xticks = [1, 10, 20, 30, 40, 50])
+    # data_of_psw(main_dir/"reports-n63", "runtime_of_psw-n63.pdf", xticks = [2, 200, 400, 600, 800, 1000, 1200, 1400])
+    # ----- set: bm-n-63; memory -----
+    # data_of_clauses(main_dir/"reports-n63", [63], 50, "memory_of_clauses-n63.pdf", data_type = DataType.memory, xticks = [1, 10, 20, 30, 40, 50])
+    # data_of_psw(main_dir/"reports-n63", "memory_of_psw-n63.pdf", data_type = DataType.memory, xticks = [2, 200, 400, 600, 800, 1000, 1200, 1400])
